49dedba4d405e1d8cc11580c56da0badac5df7bc
Artsiukh Daniil <artjx_da_22@mf.grsu.by>
2025-02-13 19:56:59 +0300

    Second version of initial script + DbModel

--- Sql_lnitial_Script.sql
+++ Sql_lnitial_Script.sql
@@ -1,51 +1,59 @@
-CREATE TYPE SupervisorTypeEnum AS ENUM ('FromOrganization', 'FromUniversity');
-CREATE TYPE TraineeshipStatusEnum AS ENUM ('planned', 'in_progress', 'completed', 'canceled');
+CREATE TABLE SupervisorType (
+    Id SERIAL PRIMARY KEY,
+    Name VARCHAR(25) NOT NULL UNIQUE
+);
+
+CREATE TABLE TraineeshipStatus (
+    Id SERIAL PRIMARY KEY,
+    Name VARCHAR(25) NOT NULL UNIQUE
+);
 
 CREATE TABLE Speciality (
     Id SERIAL PRIMARY KEY,
-    Name VARCHAR(120) NOT NULL
+    Name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE GroupName (
+    Id SERIAL PRIMARY KEY,
+    Name VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE Organization (
+    Id SERIAL PRIMARY KEY,
+    Name VARCHAR(70) NOT NULL
 );
 
 CREATE TABLE Student (
     Id SERIAL PRIMARY KEY,
     FullName VARCHAR(120) NOT NULL,
     Birthday DATE NOT NULL,
-    PhoneNumber VARCHAR(30) NOT NULL,
-    GroupName VARCHAR(50) NOT NULL,
-    Course INTEGER CHECK(Course > 0 AND Course < 7) NOT NULL,
-    Speciality_Id INTEGER,
-    FOREIGN KEY (Speciality_Id) REFERENCES Speciality(Id)
+    PhoneNumber VARCHAR(20),
+    Course CHAR(1) NOT NULL,
+    Speciality_Id INTEGER NOT NULL,
+    GroupName_Id INTEGER NOT NULL,
+    FOREIGN KEY (Speciality_Id) REFERENCES Speciality(Id),
+    FOREIGN KEY (GroupName_Id) REFERENCES GroupName(Id)
 ); 
 
-CREATE TABLE Organization (
+CREATE TABLE Traineeship (
     Id SERIAL PRIMARY KEY,
-    Name VARCHAR(50) NOT NULL
+    StartDate DATE NOT NULL,
+    EndDate DATE NOT NULL,
+    Status_Id INTEGER NOT NULL,
+    Student_Id INTEGER NOT NULL,
+    FOREIGN KEY (Status_Id) REFERENCES TraineeshipStatus(Id),
+    FOREIGN KEY (Student_Id) REFERENCES Student(Id),
+    CONSTRAINT chk_dates CHECK (StartDate <= EndDate)
 );
 
 CREATE TABLE TraineeshipSupervisor (
     Id SERIAL PRIMARY KEY,
     FullName VARCHAR(120) NOT NULL,
     PhoneNumber VARCHAR(20),
-    SupervizorType SupervisorTypeEnum NOT NULL
-);
-
-CREATE TABLE Traineeship (
-    Id SERIAL PRIMARY KEY,
-    StartDate DATE NOT NULL,
-    EndDate DATE NOT NULL,
-    Status TraineeshipStatusEnum NOT NULL,
-    Organization_Id INT NOT NULL,
-    TraineeshipSupervizor_Id INT NOT NULL,
-    FOREIGN KEY (Organization_Id) REFERENCES Organization(Id),
-    FOREIGN KEY (TraineeshipSupervizor_Id) REFERENCES TraineeshipSupervisor(Id)
-);
-
-CREATE TABLE Student_Traineeship (
-    Student_Id INTEGER NOT NULL,
-    Traineeship_Id INTEGER NOT NULL,
-    PRIMARY KEY (Student_Id, Traineeship_Id),
-    FOREIGN KEY (Student_Id) REFERENCES Student(Id),
-    FOREIGN KEY (Traineeship_Id) REFERENCES Traineeship(Id)
+    SupervisorType_Id INTEGER NOT NULL,
+    Organization_Id INTEGER,
+    FOREIGN KEY (SupervisorType_Id) REFERENCES SupervisorType(Id),
+    FOREIGN KEY (Organization_Id) REFERENCES Organization(Id)
 );
 
 CREATE TABLE Traineeship_TraineeshipSupervisor (
@@ -54,20 +62,4 @@ CREATE TABLE Traineeship_TraineeshipSupervisor (
     PRIMARY KEY (Traineeship_Id, TraineeshipSupervisor_Id),
     FOREIGN KEY (Traineeship_Id) REFERENCES Traineeship(Id),
     FOREIGN KEY (TraineeshipSupervisor_Id) REFERENCES TraineeshipSupervisor(Id)
-);
-
-CREATE TABLE Organization_TraineshipSupervisor (
-    Organization_Id INTEGER NOT NULL,
-    TraineeshipSupervisor_Id INTEGER NOT NULL,
-    PRIMARY KEY (Organization_Id, TraineeshipSupervisor_Id),
-    FOREIGN KEY (Organization_Id) REFERENCES Organization(Id),
-    FOREIGN KEY (TraineeshipSupervisor_Id) REFERENCES TraineeshipSupervisor(Id)
-);
-
-CREATE TABLE Organization_Traineeship (
-    Organization_Id INTEGER NOT NULL,
-    Traineeship_Id INTEGER NOT NULL,
-    PRIMARY KEY (Organization_Id, Traineeship_Id),
-    FOREIGN KEY (Organization_Id) REFERENCES Organization(Id),
-    FOREIGN KEY (Traineeship_Id) REFERENCES Traineeship(Id)
 );
